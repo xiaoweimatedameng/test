bddee050b8a2af167fb6c1754d8e56d5b2ab89a5
熊小威 <2636330180@qq.com>
2020-05-20 16:16:16 +0800

    添加.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by a on 2020/5/20.
+//  Copyright © 2020 xw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
